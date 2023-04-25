8e4cfc86c1faf369259daa0e9cd5ff51dad1a929
George Krier <gkrier04@icloud.com>
2023-04-25 14:29:07 -0600

    BuildingController now Functional

--- server/src/main/java/com/ramt/resources/repository/BuildingRepository.java
+++ server/src/main/java/com/ramt/resources/repository/BuildingRepository.java
@@ -2,11 +2,11 @@ package com.ramt.resources.repository;
 
 import com.ramt.resources.model.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
-    ArrayList<Building> findAllBuilding();
 }
